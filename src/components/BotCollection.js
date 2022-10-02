b47c8dd7f80466b79dd274496551ed904c0ec334
sean-code <john.nganga@student.moringaschool.com>
2022-10-02 15:36:07 +0300

    Pass bots as props

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,20 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({bots}) {
   // Your code here
+  const robots = bots.map((bot)=>{
+    return(
+      <BotCard bot={bot}>
+      </BotCard>
+    )
+  })
+
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        <h1>Collection of all bots</h1>
+        {robots}
       </div>
     </div>
   );
