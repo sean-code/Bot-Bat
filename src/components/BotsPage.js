da4b0c00c472908ae93120088bd3f77221870543
sean-code <john.nganga@student.moringaschool.com>
2022-10-02 15:36:34 +0300

    Fetch bots from local db.json

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
+  const [bots, setBots] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:8002/bots')
+    .then((response)=> response.json())
+    .then((data)=> setBots(data));
+  }, []);
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={bots}/>
     </div>
   )
 }
