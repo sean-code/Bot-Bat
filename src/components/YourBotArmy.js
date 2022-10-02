a1421c82f47ae3e51b58aa3cb38445eb50fae359
sean-code <john.nganga@student.moringaschool.com>
2022-10-02 16:57:32 +0300

    Add the onChoose function

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import BotCard from "./BotCard";
 
 function YourBotArmy({bots}) {
   //your bot army code here...
+  function onChooseBot(bot){
+    console.log(bot)
+
+  }
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {
+            bots.map((bot)=>{
+            return(
+              <BotCard key={bot.id}>
+                onChooseBot={onChooseBot}
+                bot={bot}
+              </BotCard>
+            )
+          }
+          )}
+          <h2>Your Bot Army</h2>
         </div>
       </div>
     </div>
